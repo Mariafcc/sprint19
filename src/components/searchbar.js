556120c1a0d2d5216628419194447a07fdb3069e
Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>
2020-11-13 14:16:37 -0600

    search edited

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -4,13 +4,14 @@ const SearchBar = ({ search }) => {
     return (
         <div className="search">
             <form className="form-inline">
-                <input className="form-control" 
+                <input 
+                className="form-control" 
                 type="search" 
                 placeholder="search" 
-                onChange={event => search(event)}></input>
+                onChange={event => search(event)}/>
             </form>
         </div>
-    )
+    );
 }
 
 export default SearchBar;
