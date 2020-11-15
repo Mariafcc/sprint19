424f2017ec36f65386e48552a28699e4b4357705
Mariafcc <mariafernandacruz@MariaFernandas-MacBook-Pro.local>
2020-11-14 18:58:33 -0600

    added styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import React from "react"
+import "./styles/header.css"
 
 const Header = () => {
     return (
         <div className="header">
             <jumbotron>
                 <h1>Employee Directory</h1>
-                <p>Click the name to filter or use the search bar to narrow results</p>
+                <p>Click the name to sort alphabetically or use the search bar to narrow results</p>
             </jumbotron>
         </div>
     )
